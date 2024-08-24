54ee0490168bf003750935a504dc470a080f91e8
Umair Idrees <umairmayo607@gmail.com>
2024-08-23 23:02:50 -0700

    add  01_variable and remove test file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const umair = "name1";
+var shoaib = "name2";
+let idrees = "name3";
+name = "name4";
+
+let alias;
+
+console.log(umair, shoaib, idrees, name, alias);
+
+// umair = "1"; because this is constant we can't change the value of constant variable
+
+shoaib = "2";
+idrees = "3";
+name = "name";
+
+console.warn(umair, shoaib, idrees, name, alias);
