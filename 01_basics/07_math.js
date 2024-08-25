97efeeb59a56ca6f0c699fc01a8d2d5464e01adf
Muhammad Umair Idrees <umairtech607@gmail.com>
2024-08-25 10:19:11 -0700

    add math file

---
+++ 01_basics/07_math.js
@@ -0,0 +1,18 @@
+console.log(Math.random());
+
+console.log(Math.random() * 10 + 1);
+
+min = 23;
+max = 100;
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+// console.log(Math.floor(Math.random() *(max-min -1) +min));
+
+// console.log(Math.random() *(max-min +1) + min)
+
+console.log(Math.ceil(23.3));
+console.log(Math.floor(23.9));
+console.log(Math.round(24.6));
+
+
